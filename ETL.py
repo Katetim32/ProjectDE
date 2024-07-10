ab364ced9a6bbf66aede10a555b16fd55a569a2b
Katetim32 <ekaterinatimireva1508@gmail.com>
2024-07-11 00:15:54 +0300

    Добавила логирование в таблицу logs в базу

--- ETL.py
+++ ETL.py
@@ -1,11 +1,12 @@
 import json
+import time
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import insert
-
+import socket
 def load_config():
     with open("config/config.json") as f:
         config = json.load(f)
@@ -30,27 +31,64 @@ def custom_drop_duplicates(df, subset=None):  # функция для удале
 
     return pd.concat([non_nan_duplicates, nan_rows], ignore_index=True)
 
+
+# функция для логирования
+def log_to_db(message, engine, tablename, operation, status, client_addr, client_hostname):
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal()
+    meta = db.MetaData('logs')
+    table_for_log = db.Table('logs', meta, autoload_with=engine, schema='logs')
+    log_entry = {
+        'message': message,
+        'date_start': pd.Timestamp.now(),
+        'tablename': tablename,
+        'operation': operation,
+        'status': status,
+        'client_addr': client_addr,
+        'client_hostname': client_hostname
+    }
+    session.execute(table_for_log.insert().values(log_entry))
+    session.commit()
+    session.close()
+
+
 def load_to_db(df, table_name, schema):
     # подключение к базе данных на локальном компьютере
     password = load_config()["password"]
     database_loc = f"postgresql://postgres:{password}@localhost:5432/postgres"
     engine = create_engine(database_loc)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    session = SessionLocal()
-    df.columns = df.columns.str.lower()# преобразование полей датафреймов в нижний регистр (чтобы совпадали с полями в базе)
-    df.replace({np.nan: None}, inplace=True)
-    data_list = df.to_dict(orient='records')
-    meta = db.MetaData(schema)
-    table = db.Table(table_name, meta, autoload_with=engine, schema=schema)
-    primary_keys = [key.name for key in table.primary_key] #первичные ключи таблицы
-    # вставка данных в таблицу, или обновление при конфликте
-    stmt = insert(table).values(data_list).on_conflict_do_update(
-        index_elements=primary_keys,
-        set_={col.name: col for col in insert(table).excluded if col.name not in primary_keys}
-    )
-    session.execute(stmt)
-    session.commit()
-    session.close()
+
+    client_hostname = socket.gethostname()
+    client_addr = socket.gethostbyname(client_hostname)
+
+    log_to_db(f"Начало загрузки данных в таблицу {table_name}", engine, table_name, 'UPSERT', 'started',
+              client_addr, client_hostname)
+
+    try:
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        session = SessionLocal()
+        df.columns = df.columns.str.lower()# преобразование полей датафреймов в нижний регистр (чтобы совпадали с полями в базе)
+        df.replace({np.nan: None}, inplace=True)
+        data_list = df.to_dict(orient='records')
+        meta = db.MetaData(schema)
+        table = db.Table(table_name, meta, autoload_with=engine, schema=schema)
+        primary_keys = [key.name for key in table.primary_key] #первичные ключи таблицы
+        # вставка данных в таблицу, или обновление при конфликте
+        stmt = insert(table).values(data_list).on_conflict_do_update(
+            index_elements=primary_keys,
+            set_={col.name: col for col in insert(table).excluded if col.name not in primary_keys}
+        )
+        session.execute(stmt)
+        session.commit()
+        session.close()
+        time.sleep(5)
+        log_to_db(f"Окончание загрузки данных в таблицу {table_name}", engine, table_name, 'UPSERT', 'succeed',
+                  client_addr, client_hostname)
+
+    except Exception as e:
+        log_to_db(f"Ошибка при загрузке данных в таблицу {table_name}: {str(e)}", engine, table_name, 'UPSERT',
+                  'failed', client_addr, client_hostname)
+
 
 # создание датафреймов
 md_ledger_account_s = pd.DataFrame()
@@ -68,6 +106,7 @@ md_account_d = extract('files/md_account_d.csv', delimiter=';')
 md_currency_d = extract('files/md_currency_d.csv', delimiter=';', encoding='cp866')
 md_exchange_rate_d = extract('files/md_exchange_rate_d.csv', delimiter=';')
 
+# запихнуть в функцию Transform ?
 # Transform - преобразование данных
 # удаление столбца последовательности для 5 датафреймов,
 # во фрейме ft_posting_f этот столбец нужен для составного ключа
